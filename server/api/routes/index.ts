608e2f544caf112634c1e564e5781868a3ffdbe8
Daniel Guillen <danielguillencoco@yahoo.com>
2024-10-01 21:14:30 -0500

    building test controller and services

--- server/api/routes/index.ts
+++ server/api/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import testRoute from "./testRoute";
 
 export default (): Router => {
   const app = Router();
+  // API's here
 
+  testRoute(app)
   return app;
 };
--- server/api/routes/testRoute.ts
+++ server/api/routes/testRoute.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import controllers from "../controllers";
 
 const route = Router();
 
 export default (app: Router) => {
-  app.use("/test", route);
+  app.use("/test", route); //  localhost:4000/api/test
 
-  route.get("/");
+  route.get("/", controllers.testController.testControllerFunction);
+  "localhost:4000/api/test"
+
+  route.get("/pokemon", controllers.testController.testControllerFunction);
+  "localhost:4000/api/test/pokemon"
 };
